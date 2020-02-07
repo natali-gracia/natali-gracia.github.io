5a4f83a6c47c08ea584aef82cd457fd7c15ac516
Gracia <natali1gracia@gmail.com>
2020-02-07 19:29:28 +0200

    add product-quantity

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 // Buy Button - Event - add-to-card
 
 let productCountEl = document.getElementById("product-count");
-console.log(productCountEl);
-
 let addToCardButtons = document.querySelectorAll(".add-to-card")
-console.log(addToCardButtons);
 
 for (let i = 0;i<addToCardButtons.length; i++) {
 	addToCardButtons[i].addEventListener("click",function() {
@@ -21,7 +18,6 @@ $('.slick').slick({
 // Wish Button - Event - Add-to-favorite
 
 let addToFavorites = document.querySelectorAll(".icon-favorites");
-console.log(addToFavorites);
 
 for (let i = 0;i<addToFavorites.length; i++) {
 	addToFavorites[i].addEventListener("click", function() {
@@ -30,3 +26,46 @@ for (let i = 0;i<addToFavorites.length; i++) {
   			} else this.classList.add("liked");
 	})
 }
+
+// Product-quantity on catalog item
+
+let decrementButtons = document.querySelectorAll(".decrement-button");
+let incrementButtons = document.querySelectorAll(".increment-button");
+let productQuantityInput = document.querySelectorAll(".product-quantity-input");
+
+
+for (let i = 0;i<incrementButtons.length; i++) {
+  incrementButtons[i].addEventListener("click", function() {
+      let currentValue = +productQuantityInput[i].value;
+      let nextValue = currentValue + 1;
+      productQuantityInput[i].value = nextValue;
+
+      disableEnableButtons(nextValue,i); 
+  })
+}
+
+for (let i = 0;i<decrementButtons.length; i++) {
+  decrementButtons[i].addEventListener("click", function() {
+  		let currentValue = +productQuantityInput[i].value;
+  		let nextValue = currentValue - 1;
+  		productQuantityInput[i].value = nextValue;
+
+      disableEnableButtons(nextValue,i);
+  })
+}
+
+function disableEnableButtons(count,a) {
+    if (count <=1) {
+        decrementButtons[a].disabled = true;
+        incrementButtons[a].disabled = false;
+      } 
+      else if (count >=10) {
+        decrementButtons[a].disabled = false;
+        incrementButtons[a].disabled = true;
+      } else {
+        incrementButtons[a].disabled = false;
+        decrementButtons[a].disabled = false;
+      }
+  } 
+
+
